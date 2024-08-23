1c4537de4dc3afcdfe7b4ffdf3e0b111480bcaf7
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2024-08-23 11:38:40 +0200

    tets: testing socket in deploy environment

--- server.js
+++ server.js
@@ -9,22 +9,22 @@ const socketIo = require("socket.io");
 const io = socketIo(server, {
   cors: {
     origin: process.env.ORIGIN,
-    methods: ["GET", "POST"], //TODO add other methods if needed like DELETE
+    methods: ["GET", "POST"],
   },
 });
 
 io.on('connection', (socket) => {
-  // console.log('New client connected');
+  console.log('New client connected');
 
   // Join a room based on the event or car group ID
   socket.on('joinRoom', (eventOrCarGroupId) => {
     socket.join(eventOrCarGroupId);
-    // console.log(`User joined room: ${eventOrCarGroupId}`);
+    console.log(`User joined room: ${eventOrCarGroupId}`);
   });
 
   // Handle incoming chat messages
   socket.on('chat message', (message) => {
-
+    console.log("transmitting new message to everyone in chat")
     // Broadcast to all connected clients to the same chat group (event or car group ID) that a message was created
     io.to(message.relatedId).emit('chat message', message); 
   });
@@ -35,9 +35,9 @@ io.on('connection', (socket) => {
     io.to(message.relatedId).emit('message delete', message); 
   })
 
-  // socket.on('disconnect', () => {
-  //   console.log('Client disconnected');
-  // });
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
 });
 
 server.listen(PORT, () => {
